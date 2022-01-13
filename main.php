b59554a6dbb76624a1dbfa238235671f7bf3aa44
CarlosRivera07 <42561117+CarlosRivera07@users.noreply.github.com>
2022-01-13 12:04:49 -0700

    prueba 2

--- main.php
+++ main.php
@@ -1,3 +1,10 @@
-<?php echo 'Hello World';
-echo 'hola mundo cruel hola';
+<?php 
+    $boton1="";
+
+    if(isset($_POST['boton1']))$boton1=$_POST['boton1'];
+
+    if($boton1){
+        echo "Usted ha pulsado el boton 1";
+    }
+
 ?>|
